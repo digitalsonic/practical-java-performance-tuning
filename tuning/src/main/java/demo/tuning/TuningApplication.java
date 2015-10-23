3966d04456094336a217e58b7fd684ef1cf3a5de
dingxuefeng <dingxuefeng@baidu.com>
2015-10-23 15:14:38 +0800

    init demo spring boot project

---
+++ tuning/src/main/java/demo/tuning/TuningApplication.java
@@ -0,0 +1,12 @@
+package demo.tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuningApplication.class, args);
+    }
+}
